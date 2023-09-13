28f643ba9a05faac22875a8596aef3004ca533c8
Rameez <shaikhrameez516@gmail.com>
2023-09-13 18:56:00 +0530

    commit

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -13,13 +13,13 @@ const Sidebar = () => (
 
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname='active' to='/'>
+            <NavLink exact="true" activeclassname='active' to='/rameez-portfolio'>
                 <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
             </NavLink>
-            <NavLink exact="true" activeclassname='active'className='about-link' to='/about'>
+            <NavLink exact="true" activeclassname='active'className='about-link' to='/rameez-portfolioabout'>
                 <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
             </NavLink>
-            <NavLink exact="true" activeclassname='active' className='contact-link' to='/contact'>
+            <NavLink exact="true" activeclassname='active' className='contact-link' to='/rameez-portfolio/contact'>
                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
             </NavLink>
         </nav>
